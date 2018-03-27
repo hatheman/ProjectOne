052b42c1d52ab8f04108ff20a03d94163faa3d16
Min Soo Ha <minsooha@Mins-MacBook-Pro.local>
2018-03-26 17:10:06 -0700

    <final>

--- assets/censusApi.js
+++ assets/censusApi.js
@@ -183,7 +183,7 @@ function getPopulation(response){
                 console.log(datas2015)
                 insertChart();
                 createChart(labelsOrigin,datas, datas2015);
-              },3000);
+              },5000);
             
         })
         
